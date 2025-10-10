adb7fd5d06416daed8ef43183ea68a8307fe250d
dpbs <840886454@qq.com>
2025-10-10 17:41:57 +0800

    feat: /ui/foma 编辑器

---
+++ packages/ui/src/components/CommonFoma/src/Foma.types.ts
@@ -0,0 +1,4 @@
+export interface FomaProps {
+  allowedVars?: string[];
+  allowedFuns?: string[];
+}
--- playground/src/views/Demo/index.vue
+++ playground/src/views/Demo/index.vue
@@ -1,14 +1,43 @@
 <script setup lang="ts">
 import { CommonFoma } from '~/dlib-ui';
 import { ref } from 'vue';
-const model = ref('a');
-function errorFun(e) {
-  console.log(e);
+const model = ref('');
+const error = ref('');
+const CommonFomaRef = ref();
+
+const funs = ['SUM', 'MIX'];
+const vars = ['变量1', '变量2'];
+function insertFun(item: string) {
+  CommonFomaRef.value.insertFunction(item, ['1', '2']);
+}
+function insertVar(item: string) {
+  CommonFomaRef.value.insertVariableBlock(item);
 }
 </script>
 
 <template>
-  <CommonFoma v-model="model" :variables="[{ name: 'a' }, { name: 'b' }]" @error="errorFun" />
+  <button v-for="item in vars" :key="item" @click="insertVar(item)">
+    {{ item }}
+  </button>
+  <button v-for="item in funs" :key="item" @click="insertFun(item)">
+    {{ item }}
+  </button>
+  {{ model }}
+  <CommonFoma
+    ref="CommonFomaRef"
+    v-model="model"
+    v-model:error="error"
+    :allowed-funs="funs"
+    :allowed-vars="vars"
+  />
+  <div v-if="error" class="error">
+    {{ error }}
+  </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.error {
+  margin-top: 5px;
+  color: var(--el-color-error);
+}
+</style>
